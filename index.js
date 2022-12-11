67bf2469dc4d86c5871aa6de65e26e58e63adf8d
JChase2 <mr.jdchase@gmail.com>
2022-12-10 19:36:09 -0500

    Restricting bot to music channel on discord, cleaning up console.logs and adding new shorter ones.

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ const pool = new Pool({
 
 const createNewPost = async (userName, url, topicId) => {
     try {
+        console.log("Creating a new post on DnE Forums.")
         const message = `Posted by ${userName} in DnE Discord Music Channel: [video][url]${url}[/url][/video]`;
         const text = 'INSERT INTO posts(poster, poster_id, poster_ip, message, hide_smilies, posted, topic_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *';
         const values = ['dneBot', process.env.FORUM_USER_ID, '127.0.0.1', message, 0, Math.floor(+new Date() / 1000), topicId];
         let result = await pool.query(text, values);
-        console.log("RESULT FROM CREATE NEW POST: ", result.rows[0])
         let topicReplies = await countTopicReplies(topicId);
         await updateTopic(topicReplies, result.rows[0]['posted'], result.rows[0]['id'], result.rows[0]['poster'], result.rows[0]['topic_id']);
         const topicData = await pool.query(`SELECT * FROM topics WHERE id = ${topicId}`)
-        console.log("TOPIC DATA ID: ", topicData.rows[0]['id']);
         await syncForum(topicData.rows[0]['id']);
+        console.log("Post Created.");
     } catch (err) {
         console.log("ERROR: ", err.message)
     }
@@ -34,7 +34,6 @@ const countTopicReplies = async (topicId) => {
     const values = [topicId]
     try {
         let res = await pool.query(text, values);
-        console.log("SENDING BACK FROM COUNT TOPIC REPLIES: ", res.rows[0].count);
         return parseInt(res.rows[0].count);
     } catch (err) {
         console.log("Count Topic Replies Error: ", err.message)
@@ -44,12 +43,8 @@ const countTopicReplies = async (topicId) => {
 const updateTopic = async (numReplies, lastPostTime, lastPostId, lastPosterUsername, topicId) => {
     const text = `UPDATE topics SET num_replies = $1, last_post = $2, last_post_id = $3, last_poster = $4 WHERE id = $5`;
     const values = [numReplies, lastPostTime, lastPostId, lastPosterUsername, topicId];
-
-    console.log("UPDATE TOPIC VALUES: ", values)
-
     try {
         let res = await pool.query(text, values);
-        console.log("SENDING BACK THIS RES FROM UPDATE TOPIC: ", res)
         return res;
     } catch (err) {
         console.log("Update Topic Error: ", err.message)
@@ -59,18 +54,17 @@ const updateTopic = async (numReplies, lastPostTime, lastPostId, lastPosterUsern
 // Update posts, topics, last_post, last_post_id and last_poster for a forum
 const syncForum = async (forumId) => {
     try {
+        console.log("Syncing the forum.")
         // Get the num_posts and num_topics.
         const text = `SELECT COUNT(t.id) AS num_topics, SUM(t.num_replies) AS num_posts FROM topics AS t WHERE t.forum_id = $1`
         const values = [forumId];
         let result = await pool.query(text, values);
-        console.log("RESULT 1 IN SYNC: ", result);
         // Total number of posts. Opening post + replies.
         let numPosts = result.rows[0]['num_posts'] + result.rows[0]['num_topics'];
         // Get last_post, last_post_id and last_poster for the forum (if any);
         const text2 = `SELECT t.last_post, t.last_post_id, t.last_poster FROM topics AS t WHERE t.forum_id = $1 AND t.moved_to is NULL ORDER BY t.last_post DESC LIMIT 1`;
         const values2 = [forumId];
         const result2 = await pool.query(text2, values2);
-        console.log("RESULT 2 IN SYNC: ", result2)
         // Run query to update the forum.
         const text3 = `UPDATE forums SET num_topics = $1, num_posts = $2, last_post = $3, last_post_id = $4, last_poster = $5 WHERE id = $6`;
         const values3 = [result.rows[0]['num_topics'], numPosts, result2.rows[0]['last_post'], result2.rows[0]['last_post_id'], result2.rows[0]['last_poster'], forumId];
@@ -99,8 +93,12 @@ client.on('ready', () => {
 
 // We're only interested in posting from the music disc channel to the music thread on the forums for now.
 client.on('messageCreate', async msg => {
-    if (msg.content.startsWith('https://www.youtube.com') || msg.content.startsWith('https://youtube.com') || msg.content.startsWith('https://youtu.be')) {
-        createNewPost(msg.author.username, msg.content, 35);
+    if (msg.channelId == '376566888857206785') {
+        if (msg.content.startsWith('https://www.youtube.com') || msg.content.startsWith('https://youtube.com') || msg.content.startsWith('https://youtu.be')) {
+            createNewPost(msg.author.username, msg.content, 35);
+        }
+    } else {
+        return;
     }
 });
 
